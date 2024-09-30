a97778cbad180b604bf84c2613dac914bf856f5e
Kashkumar <kash.singh1989@gmail.com>
2024-09-30 13:59:07 +0100

    Initial commit of URL shortener service

--- app/Http/Controllers/UrlShortnerController.php
+++ app/Http/Controllers/UrlShortnerController.php
@@ -3,12 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Cache;
 
 class UrlShortnerController extends Controller
 {
     private $urlKeyPrefix = 'url:';  // Prefix for cache keys
     private $shortUrlBase = 'http://short.est/'; // Base URL for short URLs
-    private $urlStore = []; // In-memory storage for URLs
+    //private $urlStore = []; // In-memory storage for URLs
 
     public function encode(Request $request)
     {
@@ -20,8 +22,18 @@ class UrlShortnerController extends Controller
         // Generate a unique short code
         $shortCode = $this->generateShortCode();
 
+        // Store the original URL in cache with the generated short code
+        Cache::put($this->urlKeyPrefix . $shortCode, $request->url, now()->addDays(7)); // Set expiration time as needed
+
         // Store the original URL with the generated short code in memory
-        $this->urlStore[$shortCode] = $request->url;
+        // $this->urlStore[$shortCode] = $request->url;
+
+         // Log the details of the encoded URL
+         Log::info('Encoded URL:', [
+            'shortCode' => $shortCode,
+            'originalUrl' => $request->url,
+        ]);
+
 
         // Return the shortened URL
         return response()->json([
@@ -39,14 +51,22 @@ class UrlShortnerController extends Controller
         // Extract the short code from the short URL
         $shortCode = basename($request->short_url);
 
+        // Retrieve the original URL from cache using the short code
+        $originalUrl = Cache::get($this->urlKeyPrefix . $shortCode);
+
         // Retrieve the original URL from memory using the short code
-        $originalUrl = $this->urlStore[$shortCode] ?? null;
+        //$originalUrl = $this->urlStore[$shortCode] ?? null;
 
         if (!$originalUrl) {
             return response()->json([
                 'error' => 'URL not found or expired'
             ], 404);
         }
+
+         // Log the original URL being returned
+         Log::info('Original URL found:', [
+            'originalUrl' => $originalUrl
+        ]);
 
         // Return the original URL
         return response()->json([
